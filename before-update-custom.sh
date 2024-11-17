55178054ef01b115e75426913cbd3dc1c157370c
Williams3993 <162794307+Williams3993@users.noreply.github.com>
2024-11-17 16:02:54 +0800

    Update before-update-custom.sh

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -9,4 +9,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default   
- echo "src-git smpackage https://github.com/kenzok8/small-package" >> ./feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
