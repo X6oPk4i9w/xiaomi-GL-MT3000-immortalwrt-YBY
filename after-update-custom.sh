dbaaa2347f1bf25f3c26fb004d1fd876672a1625
X6oPk4i9w <junxilu2021@gmail.com>
2024-11-19 20:07:14 +0800

    Update after-update-custom.sh

--- after-update-custom.sh
+++ after-update-custom.sh
@@ -2,10 +2,10 @@
 # Description: (After Update feeds)
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # Set DHCP IP Address start and end
 #sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
 #sed -i "s/option limit.*/option limit \'55\'/g" package/network/services/dnsmasq/files/dhcp.conf
 # sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/immortalwrt-AX3000T-PPPOE/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/immortalwrt-GL-MT3000/g' package/base-files/files/bin/config_generate
